2d1b4d2e97ec74762fbf53ecaedb631c1ad61a51
ornab <hzamil17@gmail.com>
2017-06-08 03:29:09 +0600

    modified

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,29 @@ const { mix } = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   mix.js('resources/assets/js/app.js', 'public/js');
+   mix.sass('resources/assets/sass/app.scss', 'public/css');
+
+
+mix.styles([
+    
+    'resources/assets/css/libs/blog-post.css',
+    'resources/assets/css/libs/bootstrap.css',
+    'resources/assets/css/libs/font-awesome.css',
+    'resources/assets/css/libs/metisMenu.css',
+    'resources/assets/css/libs/sb-admin-2.css',
+    'resources/assets/css/libs/styles.css'
+    
+], 'public/css/libs.css');
+
+mix.scripts([ 
+    
+    'resources/assets/js/libs/jquery.js'
+    'resources/assets/js/libs/bootstrap.js',
+    'resources/assets/js/libs/metisMenu.js',
+    'resources/assets/js/libs/sb-admin-2.js',
+    'resources/assets/js/libs/scripts.js'
+    
+    
+    
+], 'public/js/libs.js'); 
