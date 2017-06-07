5d0527ca2fe356e560158dce81a21305cbb54e03
ornab <hzamil17@gmail.com>
2017-06-08 00:22:36 +0600

    Modified

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,15 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('/admin/users', 'AdminUsersController');
+
+
+
+Route::get('/admin', function(){
+    
+    
+    return view('admin.index');
+    
+    
+});
