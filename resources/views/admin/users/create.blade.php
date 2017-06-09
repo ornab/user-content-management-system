7f0ae1d758c3189507da8ab1bb2333e778e0f036
ornab <hzamil17@gmail.com>
2017-06-09 12:26:15 +0600

    modified

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,15 +1,45 @@
 @extends('layouts.admin')
 
 
+
+
 @section('content')
 
-
-    
-    
-    
-    <h1>Create Users</h1>
-
-
+   <div class="container">
+       
+       <div class="row">
+           <div class="col-md-4">
+               
+                
+                    <h1 class="alert alert-success"  style="text-align:center; color:#123456" >Create Users</h1>
+             
+               
+               <div class="form-group">
+                   
+                   {!! Form::open(['method'=>'POST', 'action'=>'AdminUsersController@store'])!!}
+                   
+                   
+                   
+                   {!! Form::label('name', 'Name:')!!}
+                   {!! Form::text('name', null, ['class'=> 'form-control'])!!}
+                   
+                </div>
+                  
+                  <div class="form-group">
+                   
+                   {!! Form::submit('Create User',['class'=> 'btn btn-primary'])!!}
+                   
+                </div>
+                  
+                   {!! Form::close()!!}
+                   
+               </div>
+               
+               
+           </div>
+       </div>
+       
+   </div>
 
 
 @endsection
