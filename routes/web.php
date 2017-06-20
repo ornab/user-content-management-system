55ef002a9b405eaf357386eee97af78295d29845
ornab <hzamil17@gmail.com>
2017-06-20 12:17:36 +0600

    modified

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,10 @@ Route::group(['middleware'=>'admin'], function(){
     
     Route::resource('/admin/media', 'AdminMediaController');
     
+    Route::resource('/admin/comments', 'PostCommentsController');
+    
+    Route::resource('/admin/comment/replies', 'CommentRepliesController');
+    
    // Route::get('/admin/media/upload', ['as'=>'media.upload', 'uses'=>'AdminMediaController@store']);
     
     
