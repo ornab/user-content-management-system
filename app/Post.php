43882e6d436d61dce970b9c94cefd739be8d1b33
ornab <hzamil17@gmail.com>
2017-06-21 11:36:53 +0600

    modified

--- app/Post.php
+++ app/Post.php
@@ -34,6 +34,12 @@ class Post extends Model
         return $this->belongsTo('App\Category');
         
     }
+    
+    public function comments(){
+        
+        return $this->hasMany('App\Comment');
+         
+    }
 
 
 
