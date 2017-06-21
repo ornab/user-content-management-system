15707f21d46e15f7bedaab80f068ea64101bec06
ornab <hzamil17@gmail.com>
2017-06-21 12:19:51 +0600

    modified

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+   
+    protected $fillable = [
+        
+        'post_id',
+        'is_active',
+        'author',
+        'body',
+        'email'
+        
+        
+    ];
+    
+    
+    
+    public function replies(){
+        
+        
+        return $this->hasMany('App\CommentReply');
+        
+    }
 }
--- app/CommentReply.php
+++ app/CommentReply.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class CommentReply extends Model
 {
-    //
+    protected $fillable = [
+        
+        'post_id',
+        'is_active',
+        'author',
+        'body',
+        'email'
+        
+        
+    ];
+    
+    
+    public function comment(){
+        
+        
+        return $this->belongsTo('App\Comment');
+        
+    }
+    
 }
