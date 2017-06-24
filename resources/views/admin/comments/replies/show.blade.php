d54b24cd9167cdaa02762602376faf5bc1abe7ee
ornab <hzamil17@gmail.com>
2017-06-24 19:26:09 +0600

    modified

--- resources/views/admin/comments/replies/show.blade.php
+++ resources/views/admin/comments/replies/show.blade.php
@@ -46,9 +46,8 @@
                   {!! Form::open(['method'=>'PATCH', 'action'=>['CommentRepliesController@update', $reply->id]]) !!}
                   
                      <input type="hidden" name="is_active" value="0">
-                  
-                       
-                      <div class="form-group">
+                      
+                     <div class="form-group">
                           
                           {!! Form::submit('Un-Approve', ['class'=>'btn btn-info']) !!}
                           
